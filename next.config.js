bd216521a20cc539e0cb47c4062d7d9d9fcd071f
Sabas Segovia <sasegovia@ucdavis.edu>
2022-10-06 21:09:26 -0700

    fea: reverting back

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
+  
 }
 
 module.exports = nextConfig
